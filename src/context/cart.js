090d01fd7080e8b1b53a7afca91c01d4fabeb6ab
hyojelee <hyojelee1004ss@gmail.com>
2020-05-14 12:55:54 -0400

    cart context,caritem

--- src/context/cart.js
+++ src/context/cart.js
@@ -1,1 +1,54 @@
-// cart context
+import React from "react";
+import localCart from "../utils/localCart";
+
+const CartContext = React.createContext();
+
+function CartProvider({ children }) {
+  const [cart, setCart] = React.useState(localCart);
+  const [total, setTotal] = React.useState(0);
+  const [cartItems, setCartItems] = React.useState(0);
+
+  React.useEffect(() => {
+    //local storage
+    let newCartItems = cart.reduce((total, cartItem) => {
+      return (total += cartItem.amount);
+    }, 0);
+    setCartItems(newCartItems);
+    //total cart
+    let newTotal = cart.reduce((total, cartItem) => {
+      return (total += cartItem.amount * cartItem.price);
+    }, 0);
+    newTotal = parseFloat(newTotal.toFixed(2));
+    setTotal(newTotal);
+  }, [cart]);
+
+  //remove
+  const removeItem = (id) => {};
+  //increase
+  const increaseAmount = (id) => {};
+  //decrease
+  const decreaseAmount = (id) => {};
+  //add cart
+  const addToCart = (product) => {};
+  //clear cart
+  const clearCart = (id) => {};
+
+  return (
+    <CartContext.Provider
+      value={{
+        cart,
+        total,
+        cartItems,
+        removeItem,
+        increaseAmount,
+        decreaseAmount,
+        addToCart,
+        clearCart,
+      }}
+    >
+      {children}
+    </CartContext.Provider>
+  );
+}
+
+export { CartContext, CartProvider };
--- src/pages/ProductDetails.js
+++ src/pages/ProductDetails.js
@@ -1,7 +1,41 @@
 import React from "react";
 import { useParams } from "react-router-dom"; // to generate each url of product
-
+import { ProductContext } from "../context/products";
+// import { CartContext } from '../context/cart'
+import { useHistory } from "react-router-dom";
+import Loading from "../components/Loading";
 export default function ProductDetails() {
   const { id } = useParams();
-  return <h1>hello from product details page {id}</h1>;
+  const history = useHistory();
+  const { products } = React.useContext(ProductContext);
+  const product = products.find((item) => item.id === parseInt(id));
+
+  if (products.length === 0) {
+    return <Loading />;
+  } else {
+    const {
+      image: { url },
+      title,
+      price,
+      description,
+    } = product;
+    return (
+      <section className="single-product">
+        <img src={url} alt={title} className="single-product-image"></img>
+        <article>
+          <h1>{title}</h1>
+          <h2>${price}</h2>
+          <h3>{description}</h3>
+          <button
+            className="btn btn-primary btn-block"
+            onClick={() => {
+              history.push("/cart");
+            }}
+          >
+            add to cart
+          </button>
+        </article>
+      </section>
+    );
+  }
 }
